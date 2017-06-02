cfb003813fbce44c5a942e7130267e874b35adfd
Rushabh Shah <rushabh13shah@gmail.com>
2017-06-02 08:42:29 -0400

    Initial commit for LeetCode problems

---
+++ leetcode.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    
+    list = null
+    def __init__(self):
+        
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for x in nums:
+            print x
+temp = new Solution();
+temp.twoSum([1, 2, 3, 4, 5, 6, 7], 10);
